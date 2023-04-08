49b654e7d450dbe40086eb25f0efece99a529248
kanaru-ssk <2019january.0131@gmail.com>
2023-04-08 19:59:08 +0900

    add next-tweet

--- next-twitter-embed/next.config.js
+++ next-twitter-embed/next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+  images: {
+    remotePatterns: [
+      { protocol: "https", hostname: "pbs.twimg.com" },
+      { protocol: "https", hostname: "abs.twimg.com" },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
