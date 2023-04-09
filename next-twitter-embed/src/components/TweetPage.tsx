7342420eb804c7a289bcaff8288f98991845208e
kanaru-ssk <2019january.0131@gmail.com>
2023-04-10 00:42:54 +0900

    delete globals.css

---
+++ next-twitter-embed/src/components/TweetPage.tsx
@@ -0,0 +1,19 @@
+// import { useRouter } from "next/router";
+// // import { EmbeddedTweet, TweetSkeleton } from "next-tweet";
+// import type { Tweet } from "next-tweet/api";
+
+// export const TweetPage = ({ tweet }: { tweet: Tweet }) => {
+//   const { isFallback } = useRouter();
+
+//   return (
+//     <div data-theme="dark">
+//       {isFallback ? (
+//         <div>isFallback</div>
+//       ) : (
+//         // <TweetSkeleton />
+//         <div>isNotFallback</div>
+//         // <EmbeddedTweet tweet={tweet} priority />
+//       )}
+//     </div>
+//   );
+// };
